1724cd59652486ac37e7490de1bb70eabc934a8a
South <juliagetman5@gmail.com>
2019-03-29 01:54:03 +0200

    first step 2.0

--- sources/main.c
+++ sources/main.c
@@ -1,6 +1,6 @@
 #include "fdf.h"
 
-static int	validation(t_fdf **fdf)
+static int	validation(t_fdf *fdf)
 {
 	t_coord	*c;
 
@@ -9,25 +9,26 @@ static int	validation(t_fdf **fdf)
 		return (ft_strerr("Error: Invalid name of file."));
 	if ((c->x = save_absis(fdf->line)) <= 0)
 		return (ft_strerr("Error: Invalid map."));
-	*fdf = init_fdf(init_line(fdf, c), c->x, c->y);
+	*fdf = init_fdf(init_line(fdf, c), c);
 	return (0);
 }
 
 int 		main(int argc, char **argv)
 {
-    t_fdf	*fdf;
+    t_fdf	fdf;
 
 	if (argc != 2)
 		return (ft_strerr("Usage: ./fdf <filename>"));
-	fdf = (t_fdf*)malloc(sizeof(t_fdf));
-	fdf->line = argv[1];
+	fdf = (t_fdf)malloc(sizeof(t_fdf));
+	fdf.line = argv[1];
 	if (validation(&fdf))
 		return (1);
 	system("afplay -v &");
-	mlx_hook(fdf->win, WIN_HEIGHT, WIN_WIDTH,
+	mlx_win_manager(fdf);
+	mlx_hook(fdf.win, WIN_HEIGHT, WIN_WIDTH,
 				key_hook, fdf);
-	mlx_hook(fdf->win, WIN_HEIGHT, WIN_WIDTH,
+	mlx_hook(fdf.win, WIN_HEIGHT, WIN_WIDTH,
 				exit_hook, (void *)0);
-    mlx_loop(fdf->mlx);
+    mlx_loop(fdf.mlx);
     return (0);
 }
