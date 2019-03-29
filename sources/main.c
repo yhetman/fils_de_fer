ff88b4118ca074992d6eb7688d54d5188d8e2216
Yuliia HETMAN <yhetman@e2r12p3.unit.ua>
2019-03-29 21:09:02 +0200

    xy coord

--- sources/main.c
+++ sources/main.c
@@ -6,22 +6,64 @@
 /*   By: yhetman <yhetman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 11:01:34 by yhetman           #+#    #+#             */
-/*   Updated: 2019/03/29 11:27:26 by yhetman          ###   ########.fr       */
+/*   Updated: 2019/03/29 21:06:06 by yhetman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
+
+static int			save_ordinat(char *str)
+{
+	int		ordinat;
+	int		res;
+	int		fd;
+	char	*line;
+
+	ordinat = -1;
+	fd = open(str, O_RDONLY);
+	while ((++ordinat > -1 && (res = get_next_line(fd, &line))) > 0 )
+		ft_strdel(&line);
+	if (res < 0)
+		return (-1);
+	close(fd);
+	return (ordinat);
+}
+
+static int			save_absis(char *str)
+{
+	int		absis;
+	int		tmp;
+	int		i;
+	int		fd;
+	char	*line;
+
+	absis = 0;
+	fd = open(str, O_RDONLY);
+	while (get_next_line(fd, &line) && (tmp = 0) && (i = -1))
+	{
+		while (line[++i])
+			if (line[i] != ' ' && (line[i + 1] == ' ' || !line[i + 1]))
+				tmp++;
+		ft_strdel(&line);
+		if (!absis && absis != tmp)
+			absis = tmp;
+		else if (absis != tmp || !absis)
+			return (-1);
+	}
+	close(fd);
+	return (absis);
+}
 
 static int	validation(t_fdf *fdf)
 {
 	t_coord	*c;
 
 	c = (t_coord*)malloc(sizeof(t_coord));
-	//if ((c->y = save_ordinat(fdf->line)) < 0)
-	//	return (ft_strerr("Error: Invalid name of file."));
-	//if ((c->x = save_absis(fdf->line)) <= 0)
-	//	return (ft_strerr("Error: Invalid map."));
-	//*fdf = init_fdf(init_line(fdf, c), c);
+	if ((c->x = save_absis(fdf->line)) <= 0)
+		return (ft_strerr("ERROR: Invalid map."));
+	if ((c->y = save_ordinat(fdf->line)) < 0)
+		return (ft_strerr("ERROR: Invalid name of file."));
+	*fdf = init_fdf(init_line(fdf, c), c);
 	return (0);
 }
 
@@ -51,17 +93,17 @@ static void		display_menu(t_fdf *fdf)
 			0x262626, width, WIN_HEIGHT);
 	mlx_put_image_to_window(fdf->mlx, fdf->win, image->ptr, 0, 0);
 	mlx_string_put(fdf->mlx, fdf->win, 80, 10, 0xFFFFFF, "FDF Controls");
-	mlx_string_put(fdf->mlx, fdf->win, 10, 60, 0xFFFFFF, "Map:");
+	mlx_string_put(fdf->mlx, fdf->win, 10, 60, 0xFFFFFF, "MAP:");
 	mlx_string_put(fdf->mlx, fdf->win, 60, 60, 0xFFFFFF, fdf->line);
 	mlx_string_put(fdf->mlx, fdf->win, 10, 90, 0xFFFFFF,
-			"Rotation: ^ v < >");
+			"TO ROTATE USE: ^ v < >");
 	mlx_string_put(fdf->mlx, fdf->win, 10, 120, 0xFFFFFF,
-			"To zoom: MOUSE UP or MOUSE DOWN");
+			"TO ZOOM: MOUSE UP or MOUSE DOWN");
 	mlx_string_put(fdf->mlx, fdf->win, 10, 150, 0xFFFFFF,
-			"To change height: + or -");
-	mlx_string_put(fdf->mlx, fdf->win, 10, 180, 0xFFFFFF, "To exit: ESC");
+			"TO CHANGE HEIGHT: + or -");
+	mlx_string_put(fdf->mlx, fdf->win, 10, 180, 0xFFFFFF, "TO EXIT: ESC");
 	mlx_string_put(fdf->mlx, fdf->win, 10, WIN_HEIGHT - 25, 0xFFFFFF,
-			"Author: YHETMAN");
+			"AUTHOR: YHETMAN");
 	ft_memdel((void*)&image);
 }
 
@@ -88,12 +130,24 @@ static	void mlx_manager(t_fdf *fdf)
     mlx_loop(fdf->mlx);
 }
 
+void		mal_error(void)
+{
+	ft_strerr("ERROR:\t Malloc failed.\n");
+	exit(EXIT_FAILURE);
+}
+
+void		arg_error(void)
+{
+	ft_strerr("ERROR:\t Wrong number of arguments.\nUSAGE:\t ./fdf <map_filename>");
+	exit(EXIT_FAILURE);
+}
+
 int 		main(int argc, char **argv)
 {
     t_fdf	fdf;
 
 	if (argc != 2)
-		return (ft_strerr("Usage: ./fdf <filename>"));
+		arg_error();
 	fdf.line = argv[1];
 	if (validation(&fdf))
 		return (1);
