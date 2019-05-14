7d714b10349a3d31b21f82830fb9411cad8bb086
Yuliia HETMAN <yhetman@student.unit.ua>
2019-05-14 19:59:55 +0300

    changing

--- sources/draw.c
+++ sources/draw.c
@@ -6,7 +6,7 @@
 /*   By: yhetman <yhetman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 02:39:28 by yhetman           #+#    #+#             */
-/*   Updated: 2019/05/13 20:15:04 by yhetman          ###   ########.fr       */
+/*   Updated: 2019/05/14 18:03:17 by yhetman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,11 +62,11 @@ static void		draw_absis(t_fdf *f, t_algo one, t_algo two, t_line dot)
 		}
 		else
 			one.dec += one.dots->x;
-		//if (dot.height >= 0 && dot.color >= 0 &&
-		//	dot.height < WIN_WIDTH && dot.color < WIN_HEIGHT)
-		//	*(int *)(f->image.ptr + dot.color * f->image.size + dot.height
-		//	* f->image.bits) = gradient(one.shade, two.shade,
-		//	persentage(dot.height, one.c->x, two.c->x));
+		if (dot.height >= 0 && dot.color >= 0 &&
+			dot.height < WIN_WIDTH && dot.color < WIN_HEIGHT)
+			*(int *)(f->image.ptr + dot.color * f->image.size + dot.height
+			* f->image.bits) = gradient(one.shade, two.shade,
+			persentage(dot.height, one.c->x, two.c->x));
 	}
 }
 
--- sources/using_algo.c
+++ sources/using_algo.c
@@ -6,7 +6,7 @@
 /*   By: yhetman <yhetman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 01:37:58 by yhetman           #+#    #+#             */
-/*   Updated: 2019/05/13 20:26:47 by yhetman          ###   ########.fr       */
+/*   Updated: 2019/05/14 19:03:00 by yhetman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,13 @@ static t_algo	init_algo(int x, int y, int decimal)
 	return (a);
 }
 
-//static void		drawing(t_fdf *f, int i, int i, t_color **p)
-//{
-//	draw(f, init_algo(f + p[i][i].r, f
-//		+ p[i][i].g, p[j][i].dec),
-//		init_algo(f + p[j][i + 1].r, f
-//		+ p[j][i + 1].g, p[j][i + 1].dec));
-//}
+static void		drawing(t_fdf *f, int i, int j, t_color **p)
+{
+	draw(f, init_algo(f->x + p[j][i].r, f->y
+		+ p[j][i].g, p[j][i].dec),
+		init_algo(f->x + p[j][i + 1].r, f->y
+		+ p[j][i + 1].g, p[j][i + 1].dec));
+}
 
 void			using_algo(t_fdf *f, int x, int y, t_color **p)
 {
@@ -47,10 +47,7 @@ void			using_algo(t_fdf *f, int x, int y, t_color **p)
 	{
 		while (++i->x + 1 < x)
 		{
-			draw(f, init_algo(f->x + p[i->y][i->x].r, f->y
-			+ p[i->y][i->x].g, p[i->y][i->x].dec),
-			init_algo(f->x + p[i->y][i->x + 1].r, f->y
-			+ p[i->y][i->x + 1].g, p[i->y][i->x + 1].dec));
+			drawing(f, i->x, i->y, p);
 			draw(f, init_algo(f->x + p[i->y][i->x].r,
 			f->y + p[i->y][i->x].g, p[i->y][i->x].dec),
 			init_algo(f->x + p[i->y + 1][i->x].r,
@@ -62,8 +59,5 @@ void			using_algo(t_fdf *f, int x, int y, t_color **p)
 		f->y + p[i->y + 1][i->x].g, p[i->y + 1][i->x].dec));
 	}
 		while (++i->x + 1 < x)
-			draw(f, init_algo(f->x + p[i->y][i->x].r, f->y
-			+ p[i->y][i->x].g, p[i->y][i->x].dec),
-			init_algo(f->x + p[i->y][i->x + 1].r, f->y
-			+ p[i->y][i->x + 1].g, p[i->y][i->x + 1].dec));	
+			drawing(f, i->x, i->y, p);	
 }
